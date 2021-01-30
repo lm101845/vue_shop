830aee3044e6e8a1aa115e1841d34c85fae2b6b3
lm101845 <1018457683@qq.com>
2021-01-30 09:40:09 +0800

    登陆组件布局

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        登陆组件
+    <div class="login_container">
+        <!-- 登陆组件 -->
+        <div class="login_box">
+            <!-- 这个是登陆的白色的盒子 -->
+        </div>
     </div>
 </template>
 
@@ -18,4 +21,24 @@ export default {
 // 一个组件它的样式肯定是来美化自己组件结构的，不应该涉及到别人
 // 所以一定要记得加scoped，用来防止样式冲突
 // 接下来我们需要在router.js中通过路由的形式将它渲染到App根组件中
+
+.login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+}
+
+.login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    // 圆角边框
+
+    // 接下来我们要把这个盒子放到页面的正中间
+    position: absolute;
+    // 绝对定位脱标，孤魂野鬼
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+}
 </style>
