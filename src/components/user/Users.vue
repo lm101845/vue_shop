b90f9fe59d3c2c578205f43e64e3ee97df5147b1
lm101845 <1018457683@qq.com>
2021-02-02 09:28:49 +0800

    获取用户列表数据

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -28,7 +28,41 @@
 </template>
 
 <script>
-export default {}
+export default {
+  // 在发起请求之前我们先定义一些基本的代码结构
+  data() {
+    return {
+      // 我们最好把get参数定义到这里，不要直接在params里写
+      // 获取用户列表的参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total: 0
+    }
+  },
+  created() {
+    // 我们应该在created生命周期函数中发起当前组件的首屏数据请求
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo
+      })
+      // 这次请求需要携带一些相关的get参数
+      // get函数会返回一个promise对象，为简化promise操作，我们使用async，await
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败')
+      }
+      this.userList = res.data.users
+      this.total = res.data.total
+      console.log(res)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped></style>
