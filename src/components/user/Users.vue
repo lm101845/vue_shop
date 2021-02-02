cde14741dded70d47a992c3e3c0d7612a44f507c
lm101845 <1018457683@qq.com>
2021-02-02 22:15:38 +0800

    修改用户信息的操作

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -144,15 +144,29 @@
     </el-dialog>
 
     <!-- 修改用户的对话框 -->
-    <el-dialog
-      title="修改用户"
-      :visible.sync="editDialogVisible"
-      width="50%"
-    >
-      <span>这是一段信息</span>
+    <el-dialog title="修改用户" :visible.sync="editDialogVisible" width="50%" @close="editDialogClosed">
+      <!-- <span>这是一段信息</span> -->
+      <el-form
+        :model="editForm"
+        :rules="editFormRules"
+        ref="editFormRef"
+        label-width="70px"
+      >
+        <el-form-item label="用户名">
+          <el-input v-model="editForm.username" disabled></el-input>
+        </el-form-item>
+
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email"></el-input>
+        </el-form-item>
+
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="editForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="editDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="editDialogVisible = false"
+        <el-button type="primary" @click="editUserInfo"
           >确 定</el-button
         >
       </span>
@@ -240,9 +254,20 @@ export default {
         ]
       },
       // 控制修改用户对话框的显示与隐藏
-      editDialogVisible:false,
+      editDialogVisible: false,
       // 查询到的用户信息对象
-      editForm:{}
+      editForm: {},
+      //修改表单的验证规则对象
+      editFormRules: {
+        email: [
+          { required: true, message: '请输入用户邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur' }
+        ],
+         mobile: [
+          { required: true, message: '请输入用户手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
+      }
     }
   },
   created() {
@@ -317,12 +342,34 @@ export default {
     // 展示编辑用户的对话框
     async showEditDialog(id) {
       // console.log(id);
-      const {data:res} = await this.$http.get('users/' + id)
-      if(res.meta.status !== 200){
+      const { data: res } = await this.$http.get('users/' + id)
+      if (res.meta.status !== 200) {
         return this.$message.error('查询用户信息失败')
       }
       this.editForm = res.data
       this.editDialogVisible = true
+    },
+    // 监听修改用户对话框的关闭事件
+    editDialogClosed(){
+      this.$refs.editFormRef.resetFields()
+    },
+    // 修改用户信息并提交
+    editUserInfo(){
+      this.$refs.editFormRef.validate(async valid=>{
+        // console.log(valid);
+        if(!valid) return 
+        // 发起修改用户信息的数据请求
+        const {data:res} = await this.$http.put('users/' + this.editForm.id,{email:this.editForm.email,mobile:this.editForm.mobile})
+        if(res.meta.status !== 200){
+          return this.$message.error('更新用户信息失败!')
+        }
+        // 1.关闭对话框
+        this.editDialogVisible = false
+        // 2.刷新数据列表
+        this.getUserList()
+        // 3.提示修改成功
+        this.$message.success('更新用户信息成功！')
+      })
     }
   }
 }
